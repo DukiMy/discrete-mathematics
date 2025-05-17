14a2c254768eebcd80bbe6cda8ecd4e0e71cc714
Durim Miziraj <durim.miziraj@gmail.com>
2025-05-17 09:53:07 +0200

    Added example for permutations

--- permutations/permutations.c
+++ permutations/permutations.c
@@ -7,6 +7,9 @@ int main(int argc, char *argv[])
   for (int i = 0; i < argc; i++) {
     printf("argv[%d] = %s\n", i, argv[i]);
   }
+
+  printf("arg count = %d\n", argc);
+  printf("%d\n");
   return EXIT_SUCCESS;
 }
 
