96ed3855204bb35c04425a3164fcddbbae332800
Durim Miziraj <durim.miziraj@gmail.com>
2025-05-18 16:03:16 +0200

    Added rule of product project

---
+++ rule-of-product/rop.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// Create a table of products
+int main() {
+    // Take in parameters for symbols on the x axis and establish the length of that array of symbols.
+    char**
+    // Take in parameters for symbols on the y axis and establish the length of that array of symbols.
+
+    // symbols must be one column wide, answers can be two columns wide.
+
+
+    
+    // Establish width and length
+
+    return 0;
+}
